243c391513f0d33cd792295e97054781b2a5f32c
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-03-06 17:28:09 +0900

    change books route to RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/edit'
-
-  get 'books/index'
-
-  get 'books/new'
-
-  get 'books/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
 end
