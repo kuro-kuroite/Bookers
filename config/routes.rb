b17c1f4fb43737826aa5e432601f5ffb03cc82dc
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-03-06 17:26:21 +0900

    exec rails g controller Books edit index new show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'books/edit'
+
+  get 'books/index'
+
+  get 'books/new'
+
+  get 'books/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
